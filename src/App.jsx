b121f422355adfe019c0ff478ae99f6169864a6a
90yearsoldcoder <minty@bu.edu>
2024-04-20 21:26:36 -0400

    add functional sidebar nav without stytle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 import "./App.css";
+import layout from "./styles/Layout.module.css";
 
 function App() {
   return (
     <>
-      <div>APP side bar</div>
+      <div className={layout.sideBar}>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isActive ? "active" : isPending ? "pending" : ""
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/menu"
+          className={({ isActive, isPending }) =>
+            isActive ? "active" : isPending ? "pending" : ""
+          }
+        >
+          Menu
+        </NavLink>
+        <NavLink
+          to="/cart"
+          className={({ isActive, isPending }) =>
+            isActive ? "active" : isPending ? "pending" : ""
+          }
+        >
+          My Cart
+        </NavLink>
+      </div>
       <div>
         <Outlet></Outlet>
       </div>
