3098d6307b03c57f13b4d841084d89a4dadd41d2
90yearsoldcoder <minty@bu.edu>
2024-04-27 21:07:05 -0400

    styling sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 import "./App.css";
 import layout from "./styles/Layout.module.css";
+import Button from "./styles/Buttons.module.css";
 
 function App() {
   return (
@@ -9,16 +10,22 @@ function App() {
       <div className={layout.sideBar}>
         <NavLink
           to="/"
-          className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "pending" : ""
-          }
+          className={({ isActive, isPending }) => {
+            return (
+              Button.NavButton +
+              " " +
+              (isActive ? Button.active : isPending ? "pending" : "")
+            );
+          }}
         >
           Home
         </NavLink>
         <NavLink
           to="/menu"
           className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "pending" : ""
+            Button.NavButton +
+            " " +
+            (isActive ? Button.active : isPending ? "pending" : "")
           }
         >
           Menu
@@ -26,7 +33,9 @@ function App() {
         <NavLink
           to="/cart"
           className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "pending" : ""
+            Button.NavButton +
+            " " +
+            (isActive ? Button.active : isPending ? "pending" : "")
           }
         >
           My Cart
