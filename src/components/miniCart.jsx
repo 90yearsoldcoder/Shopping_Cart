47904dfacffc984798511f4d6a383e70e86914e3
90yearsoldcoder <minty@bu.edu>
2024-05-27 22:30:10 -0400

    fix bug: miniCard did not have pop-out transition when the miniCart open

--- src/components/miniCart.jsx
+++ src/components/miniCart.jsx
@@ -1,19 +1,23 @@
+import { useEffect, useState } from "react";
 import { useCart } from "../cache/cartProvider";
 import styles from "../styles/miniCart.module.css";
 import PropTypes from "prop-types";
 
 const MiniCart = ({ isActive, setIsActive }) => {
   const { cart, cartSetter } = useCart();
+  const [cartPop, setCartPop] = useState(false);
 
   function getBack() {
     setIsActive(false);
   }
 
+  useEffect(() => {
+    setCartPop(isActive);
+  }, [isActive, setCartPop]);
+
   return (
     <div className={isActive ? styles.cover : styles.deActive}>
-      <div
-        className={`${styles.miniCart} ${isActive ? styles.CartActive : ""}`}
-      >
+      <div className={`${styles.miniCart} ${cartPop ? styles.CartActive : ""}`}>
         {/* Your cart content here */}
         <button onClick={getBack}>Continue Shopping</button>
       </div>
